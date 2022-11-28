075d0e8820705f4d0b3b4e992afe2f4b4278af1e
Lexus039 <alex29091985@icloud.com>
2022-11-28 17:04:42 +0200

    scripts added

---
+++ scpripts.js
@@ -0,0 +1,17 @@
+</style>
+</head>
+<body>
+<p class="p1">&lt;!DOCTYPE html&gt;</p>
+<p class="p1">&lt;html&gt;</p>
+<p class="p1">&lt;head&gt;</p>
+<p class="p1"><span class="Apple-converted-space">    </span>&lt;meta charset=‘utf-8’&gt;</p>
+<p class="p1"><span class="Apple-converted-space">    </span>&lt;meta http-equiv=‘X-UA-Compatible’ content=‘IE=edge’</p>
+<p class="p1"><span class="Apple-converted-space">    </span>&lt;title&gt;Page Title&lt;/title&gt;</p>
+<p class="p1"><span class="Apple-converted-space">    </span>&lt;meta name=‘viewport’ content=‘width=device-width, initail-scale=1’</p>
+<p class="p1"><span class="Apple-converted-space">    </span>link rel=‘stylesheet’ type=‘text/css’ media=‘screen’ href=‘main.css’</p>
+<p class="p1"><span class="Apple-converted-space">    </span>&lt;script src=‘main.js’&gt;&lt;/script&gt;&lt;/head&gt;</p>
+<p class="p1">&lt;/head&gt;</p>
+<p class="p1">&lt;body&gt;</p>
+<p class="p2"><br></p>
+<p class="p1">&lt;/body&gt;</p>
+<p class="p1">&lt;/html&gt;</p>
